ac2c5efe378138747946710385aa745493aa99e9
masjeho <39433023+masjeho@users.noreply.github.com>
2022-01-18 08:29:57 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -3,7 +3,7 @@
 # 
 # ==================================================
 
-GitUser="syapik96"
+GitUser="masjeho"
 #wget https://github.com/${GitUser}/
 
 # initializing var
@@ -322,7 +322,7 @@ iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
 netfilter-persistent reload
 
-GitUser="syapik96"
+GitUser="masjeho"
 # download script
 cd /usr/local/sbin
 wget -O add-host "https://raw.githubusercontent.com/${GitUser}/aws/main/add-host.sh" && chmod +x add-host
